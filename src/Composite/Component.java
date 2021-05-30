686532633bb2f884b1397d112e1063aa85aa9a38
Your Name <oriya2699@gmail.com>
2021-05-30 16:25:05 +0300

    Composite

---
+++ src/Composite/Component.java
@@ -0,0 +1,10 @@
+package Composite;
+
+public interface Component {
+
+    void showComponent();
+    void setComponent();
+    void printComponent();
+    String getComponentContent();
+
+}
---
+++ src/Composite/Leafs/TextField.java
@@ -0,0 +1,41 @@
+package Composite.Leafs;
+
+import Composite.Component;
+
+// Leaf
+public class TextField implements Component {
+
+    private String content;
+
+    public TextField(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public void setComponent() {
+
+    }
+
+    @Override
+    public void showComponent() {
+
+    }
+
+    @Override
+    public void printComponent(){
+        System.out.println(content);
+    }
+
+    @Override
+    public String getComponentContent() {
+        return this.content;
+    }
+}
